a82b7b426dfb99448bad5bde23d9635a8e443907
Alessandro Colucci <alessandro.colucci@nttdata.com>
2023-01-29 17:44:14 +0000

    quasi funzionante

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
-			"sap/ui/core/mvc/Controller",
-			"sap/ui/Device",
-		], function (Controller, Device) {
-			"use strict";
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/Device"
+], function (Controller, Device) {
+	"use strict";
 
-			return Controller.extend("DEMO.DEMO.controller.Main", {
-					onInit: function () {
-						let oPurchase = this.getView().byId("EBELN");
-						oPurchase.bindElement("oDataModel>/A_PurchaseOrder('4500000011')");
-						this.oTable = this.getView().getModel("jsonModel").setProperty("/A_PurchaseOrder", "oDataModel>/A_PurchaseOrder('4500000011')");
-						}
-					});
-			});
+	return Controller.extend("DEMO.DEMO.controller.Main", {
+		onInit: function () {
+			let oPurchase = this.getView().byId("EBELN");
+			oPurchase.bindElement("oDataModel>/A_PurchaseOrder('4500000011')");
+			
+			var Json = {
+				"PurchaseOrder": 11
+			};
+			this.getView().getModel("jsonModel").setProperty("/A_PurchaseOrder", "oDataModel>/A_PurchaseOrder('4500000011')");
+		}
+	});
+});
