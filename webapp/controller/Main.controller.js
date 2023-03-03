e286506424a7e35a734bd6a3cc96d6e987d6f986
colucci.alessandro94@gmail.com <Alessandro.Colucci@vistex.com>
2023-03-03 19:44:56 +0100

    commit

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -6,14 +6,22 @@ sap.ui.define([
 
 	return Controller.extend("DEMO.DEMO.controller.Main", {
 		onInit: function () {
-			// Binding diretto con oData
+
+			// Binding diretto con oData (l'odata è presente nella business API)
 			let oPurchase = this.getView().byId("EBELN");
 			oPurchase.bindElement("oDataModel>/A_PurchaseOrder('4500000011')");
-			// Valorizzazione Modello Json con array
+
+
+			// Valorizzazione Modello Json con array con caricamento diretto in script
 			let arrayPo = [];
 			let elementPo1 = { PurchaseOrder : 11 , PurchaseOrderType : "test"};
 			arrayPo.push(elementPo1);
 			this.getView().getModel("jsonModel").setProperty("/A_PurchaseOrder", arrayPo);
+
+
+			// Valorizzazione Modello Json direttamente da file
+
+			
 		}
 	});
 });
