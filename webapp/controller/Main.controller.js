8b180cc8bb3e8fce6ca5487abd78932fbf24feed
colucci.alessandro94@gmail.com <colucci.alessandro94@gmail.com>
2023-03-03 20:12:39 +0100

    commit

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,27 +1,39 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/Device"
-], function (Controller, Device) {
-	"use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/Device"],
+  function (Controller, Device) {
+    "use strict";
 
-	return Controller.extend("DEMO.DEMO.controller.Main", {
-		onInit: function () {
+    return Controller.extend("DEMO.DEMO.controller.Main", {
+      onInit: function () {
+        // Binding diretto con oData (l'odata è presente nella business API)
+        let oPurchase = this.getView().byId("EBELN");
+        oPurchase.bindElement("oDataModel>/A_PurchaseOrder('4500000011')");
 
-			// Binding diretto con oData (l'odata è presente nella business API)
-			let oPurchase = this.getView().byId("EBELN");
-			oPurchase.bindElement("oDataModel>/A_PurchaseOrder('4500000011')");
+        // Valorizzazione Modello Json con array con caricamento diretto in script
+        let arrayPo = [];
+        let elementPo1 = { PurchaseOrder: 11, PurchaseOrderType: "test" };
+        arrayPo.push(elementPo1);
+        this.getView()
+          .getModel("jsonModel")
+          .setProperty("/A_PurchaseOrder", arrayPo);
 
+        // Valorizzazione Modello Json direttamente da file
+        var oModel = new sap.ui.model.json.JSONModel();
+		oModel.loadData("model/CarSell.json");
+        this.getView().setModel(oModel);
+        let OCarlist = this.getView().byId("idCarSellList");
+        OCarlist.setModel(oModel);
 
-			// Valorizzazione Modello Json con array con caricamento diretto in script
-			let arrayPo = [];
-			let elementPo1 = { PurchaseOrder : 11 , PurchaseOrderType : "test"};
-			arrayPo.push(elementPo1);
-			this.getView().getModel("jsonModel").setProperty("/A_PurchaseOrder", arrayPo);
-
-
-			// Valorizzazione Modello Json direttamente da file
-
-			
-		}
-	});
-});
+        OCarlist.bindItems({
+          path: "/vendite_auto",
+          template: new sap.m.ColumnListItem({
+            cells: [
+              new sap.m.Text({ text: "{marca}" }),
+              new sap.m.Text({ text: "{modello}" }),
+            ],
+          }),
+        });
+      },
+    });
+  }
+);
